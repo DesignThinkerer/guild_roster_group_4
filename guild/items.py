23b2a6b676568f69bd03cb11243972745d3275d6
Théophile Desmedt <34781168+DesignThinkerer@users.noreply.github.com>
2026-07-16 12:36:47 +0000

    Implement __lt__ method in Item class for comparison by rarity and value

--- guild/items.py
+++ guild/items.py
@@ -53,10 +53,9 @@ class Item:
         return hash((self.name, self.rarity, self.value))
 
     def __lt__(self, other: object) -> bool:
-        """TODO (Day 1): order by rarity first, then value as a tiebreaker.
-        Return NotImplemented if `other` isn't an Item.
-        """
-        raise NotImplementedError("TODO (Day 1): implement __lt__")
+        if not isinstance(other, Item):
+            return NotImplemented
+        return (self.rarity, self.value) < (other.rarity, other.value)
 
     def __bool__(self) -> bool:
         # Caveat: while this satisfy our current specs, this treats negative values as truthy too. 
