892d30270e783301767a7cb284bbdaaf15f60403
Théophile Desmedt <theophiledesmedt@gmail.com>
2026-07-23 12:09:44 +0200

    [IMP] Implement combined quest feed using itertools.chain

--- guild/quests.py
+++ guild/quests.py
@@ -33,11 +33,7 @@ def event_quests() -> Iterator[Quest]:
 # --- TODO (Day 3): combine sources with itertools.chain ---------------------
 
 def combined_quest_feed() -> Iterator[Quest]:
-    """TODO: use itertools.chain to treat the three quest sources above as
-    one continuous stream, without materializing any of them into a
-    combined list first.
-    """
-    raise NotImplementedError("TODO (Day 3): implement combined_quest_feed")
+    return itertools.chain(daily_quests(), guild_quests(), event_quests())
 
 
 # --- TODO (Day 3): an infinite source + itertools.islice --------------------
